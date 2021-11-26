e01f2b95683394e133d209c7a0d384b57c7eb375
Vandré Brunazo <vibrunazo@gmail.com>
2021-11-25 21:34:06 -0300

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <Header />
       <h1 className="App-header">Hello From React</h1>
       <h2>
-        2 + 5 is {2 + 5}
+        20 + 5 is {20 + 5}
       </h2>
     </div>
   );
