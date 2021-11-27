316f3e65ea08f0c5790de24721ffffa8938e3fc8
Vandré Brunazo <vibrunazo@gmail.com>
2021-11-27 08:50:30 -0300

    create a list with .map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <h1 className="App-header">Hello From React</h1>
-      <h2>
-        20 + 5 is {20 + 5}
-      </h2>
+      <Tasks />
     </div>
   );
 }
