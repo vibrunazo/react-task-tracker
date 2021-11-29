31119a760f2c729315be6b305b9792df15b7c2f9
Vandré Brunazo <vibrunazo@gmail.com>
2021-11-29 17:07:26 -0300

    form input state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from "react"
 
 function App() {
@@ -43,6 +44,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {
         tasks.length > 0 ? (
           <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
