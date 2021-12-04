2cc9bf09c0166a52f40b3d053df062a06b5f84e3
Vandré Brunazo <vibrunazo@gmail.com>
2021-12-04 15:59:49 -0300

    add task to server

--- src/App.js
+++ src/App.js
@@ -30,12 +30,23 @@ function App() {
   }
 
   // Add Task
-  const addTask = (task) => {
-    console.log(task)
-    const id = Math.floor(Math.random() * 10000) + 1
-    console.log(id)
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    // console.log(task)
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // console.log(id)
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
